44914064c8fc56f97ea802c162bd191fc31aa199
ML <lackova.yvette@gmail.com>
2024-02-03 19:12:24 +0100

    druha uloha

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -13,51 +13,83 @@ public class HlavniProgram {
 
     public void start() {
         zofka = new Turtle();
-        zofka.setPenColor(Color.pink);
-        nakresliPrasatko();
-        //TODO implementace domácího úkolu
+//        zofka.setPenColor(Color.pink);
+//        zofka.penUp();
+//        zofka.turnRight(180);
+//        zofka.move(50);
+//        zofka.turnRight(180);
+//        zofka.penDown();
+//        nakresliPrasatko();
+//        zofka.penUp();
+//        zofka.turnLeft(90);
+//        zofka.move(250);
+//        zofka.turnLeft(90);
+//        zofka.move(100);
+//        zofka.turnRight(90);
+        vytvorSlnko();
+
     }
+
+    private void vytvorSlnko() {
+        zofka.penDown();
+        zofka.setPenColor(Color.yellow);
+
+        int iteracia = 50;
+
+        for (int i = 0; i <= iteracia; i++) {
+            zofka.move(9);
+            if (i % 4 == 0) {
+                zofka.turnRight(90);
+                zofka.move(20);
+                zofka.turnRight(180);
+                zofka.move(20);
+                zofka.turnRight(90);
+            }
+            zofka.turnLeft((double) 360 / iteracia);
+        }
+        zofka.penUp();
+
+    }
+
 
     private void nakresliPrasatko() {
-        nakresliTelo(150);
+        nakresliTelo();
         zofka.turnLeft(210);
-        nakresliHlavu(150);
-        nakresliNohy(100);
+        nakresliHlavu();
+        nakresliNohy();
         zofka.move(150);
         zofka.turnLeft(90);
-        nakresliNohy(100);
+        nakresliNohy();
 
     }
 
-    private void nakresliTelo(int strana) {
+    private void nakresliTelo() {
         for (int i = 0; i < 5; i++) {
-            zofka.move(strana);
+            zofka.move(150);
             zofka.turnRight(90);
         }
     }
 
-    private void nakresliHlavu(int strana) {
+    private void nakresliHlavu() {
         for (int i = 0; i < 2; i++) {
-            zofka.move(strana);
+            zofka.move(150);
             zofka.turnLeft(120);
         }
     }
 
-    private void nakresliNohy(int dlzka) {
+    private void nakresliNohy() {
         zofka.turnRight(135);
         zofka.penDown();
-        zofka.move(dlzka / 2);
+        zofka.move(50.0);
         zofka.penUp();
         zofka.turnRight(180);
-        zofka.move(dlzka / 2);
+        zofka.move((double) 100 / 2);
         zofka.turnLeft(90);
         zofka.penDown();
-        zofka.move(dlzka / 2);
+        zofka.move((double) 100 / 2);
         zofka.penUp();
         zofka.turnRight(180);
-        zofka.move(dlzka / 2);
+        zofka.move((double) 100 / 2);
         zofka.turnRight(45);
     }
 }
-
-
